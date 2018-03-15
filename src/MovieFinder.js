c3788c7e4cbca8d9ba96c81727059d5c57ff41e5
Jonathan Cousins <jon.cousins@avanade.com>
2018-03-15 13:09:00 +0000

    Rename Base component to match app name

---
+++ src/MovieFinder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MovieFinder extends Component {
+  render() {
+    return (
+      <div className="MovieFinder">
+        <header>
+          <h1>Movie Finder</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default MovieFinder;
