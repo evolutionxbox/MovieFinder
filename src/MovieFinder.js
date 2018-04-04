1e4c0d684cd18a1a1c0617e88e80315dd1045c3a
Jonathan Cousins <jon.cousins@avanade.com>
2018-04-04 10:54:07 +0100

    Render each movie, and render each filter grouped by type

--- src/MovieFinder.js
+++ src/MovieFinder.js
@@ -3,15 +3,51 @@ import Header from './Header/Header';
 import Filter from './Filter/Filter';
 import Data from './fake-data';
 
-class MovieFinder extends Component {
-  render() {
-    return (
-      <div className="MovieFinder">
-        <Header title="Movie Finder" />
-        <Filter name="Genre" items={[{name: 'Hello', id: 'hello1'}]} />
-      </div>
-    );
-  }
+const filterTypes = ['Director', 'Country', 'Year', 'Genre'];
+
+const filterValues = (data, filterType) => {
+  return [...new Set(data.map(movie => movie[filterType].split(/\s?,\s?/)).reduce((acc, curr) => acc.concat(curr), []))];
+}
+
+const Movie = (data) => {
+  return (
+    <li>
+      <article className="movie">
+        <img src="//unsplash.it/100x150?random" alt="" />
+        <header>
+          <h1>{data['Title']}</h1>
+        </header>
+        <p>{data['Plot']}</p>
+        <footer>
+          <ul className="tags">
+            <li>{data['Director']}</li>
+            <li>{data['Country']}</li>
+            <li>{data['Year']}</li>
+            <li>{data['Genre']}</li>
+            <li>{data['Runtime']}</li>
+            <li>{data['imdbID']}</li>
+          </ul>
+        </footer>
+      </article>
+    </li>
+  )
+}
+
+const MovieFinder = (props) => {
+  return (
+    <div className="MovieFinder">
+      <Header title="Movie Finder" />
+
+      {filterTypes.map(filter => <Filter key={filter} name={filter} items={filterValues(Data, filter)} />)}
+
+      <main>
+        <h2>Showing {Data.length} films</h2>
+        <ul>
+          {Data.map(movie => <Movie key={movie['imdbID']} {...movie} />)}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default MovieFinder;
