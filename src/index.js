a54ff74dbb3a65d18342b05bca5099b91266cba4
Jonathan Cousins <jon.cousins@avanade.com>
2018-04-04 10:05:10 +0100

    Add some basic styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MovieFinder from './MovieFinder';
 import './styles/normalize.css';
+import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<MovieFinder />, document.getElementById('root'));
