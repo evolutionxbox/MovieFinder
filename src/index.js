c945478e7475938fd4af185d00be1c5cd4cc4d0e
Jonathan Cousins <jon.cousins@avanade.com>
2018-04-06 09:57:38 +0100

    Add react redux bindings
    Using the provider component wrap the MovieFinder component
    The store prop will be undefined at first

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import MovieFinder from './MovieFinder';
 import './styles/normalize.css';
 import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<MovieFinder />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <MovieFinder />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
