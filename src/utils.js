e2db4f26bf233cfdc61d66a18bf74bd9cac2255e
Jonathan Cousins <jon.cousins@avanade.com>
2018-04-06 09:56:15 +0100

    Array prototype is read only
    Convert concatAll, concatMap to functions which take an array as a param
    Add three new functions
    - a simple `unique` function which utilises Set
    - removeDuplicates, which filters based on the value of a specified property
    - csvLineToArray, converts a csv string (with no line-ends) to an array

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,31 @@
-Array.prototype.concatAll = function() {
-  var results = [];
-  this.forEach(function(subArray) {
-    results.push.apply(results, subArray);
-  });
+export function concatAll (array) {
+  var results = []
+  array.forEach(function(subArray) {
+    results.push.apply(results, subArray)
+  })
 
-  return results;
-};
+  return results
+}
 
-Array.prototype.concatMap = function(projectionFunctionThatReturnsArray) {
-  return this.
-    map(function(item) {
-      return projectionFunctionThatReturnsArray(item);
-    }).
-    // apply the concatAll function to flatten the two-dimensional array
-    concatAll();
-};
+export function concatMap (projectionFunctionThatReturnsArray, array) {
+  return concatAll(array.map(function(item) {
+    return projectionFunctionThatReturnsArray(item)
+  }))
+  // apply the concatAll function to flatten the two-dimensional array
+}
+
+export function unique (array) {
+  return [...new Set(array)]
+}
+
+export function removeDuplicates(array, prop) {
+  return array.filter((obj, pos, arr) => {
+    return arr.map(function thing(mapObj) {
+      return mapObj[prop]
+    }).indexOf(obj[prop]) === pos
+  })
+}
+
+export function csvLineToArray (csvLine) {
+  return csvLine.split(/\s?,\s?/)
+}
