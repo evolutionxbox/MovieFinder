230febd68151f67747e437b367d7016412a495e6
Jonathan Cousins <jon.cousins@avanade.com>
2018-04-09 14:03:36 +0100

    Connect filter component to redux
    - Dispatch an action when a user clicks a filter
    - Display which filters have been enabled

--- src/Filter/Filter.js
+++ src/Filter/Filter.js
@@ -1,17 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { enableFilter, disableFilter } from '../redux/actions'
 import Title from '../Title/Title';
 
-const Filter = ({name, items}) => {
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      if (ownProps.isEnabled) {
+        dispatch(disableFilter(ownProps.id))
+      } else {
+        dispatch(enableFilter(ownProps.id))
+      }
+    }
+  }
+}
+
+const Filter = ({type, value, isEnabled, onClick}) => {
+  const classname = isEnabled ? 'isEnabled' : ''
+  return (
+    <li className={classname} onClick={onClick}>{value}</li>
+  )
+}
+
+const FilterContainer = connect(null, mapDispatchToProps)(Filter)
+
+const propTransformSort = prop => transform => {
+  return (...props) => {
+    const [ left, right ] = props.map(prop).map(transform)
+    return ((left < right) ? -1 : ((left > right) ? 1 : 0))
+  }
+}
+
+const FilterType = ({type, items}) => {
   return (
     <div className="filter">
-      <Title title={name} level="3" />
+      <Title title={type} level="3" />
       <ul>
-        {items.map(item => {
-          return <li key={item} onClick={() => console.log(item)}>{item}</li>
-        })}
+        {
+          items
+            .sort(propTransformSort(x => x.value)(x => x.toLowerCase()))
+            .map(filter => <FilterContainer key={filter.value} {...filter} /> )
+        }
       </ul>
     </div>
   )
 }
 
-export default Filter;
+export default FilterType;
